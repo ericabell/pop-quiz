320d1ddeb2b9676f925d108898577da1c9c6e572
Eric Abell <ericabell@users.noreply.github.com>
2017-08-24 09:23:35 -0500

    First two functions done

--- quiz.js
+++ quiz.js
@@ -1,1 +1,17 @@
+let assert = require('assert');
+
 console.log('quiz 1');
+
+// QUESTION 1
+let begin = function() {
+  console.log('begin');
+}
+
+begin();
+
+let echo = function(arg) {
+  return arg;
+}
+
+let string = 'the iron yard';
+assert( echo(string) === string );
